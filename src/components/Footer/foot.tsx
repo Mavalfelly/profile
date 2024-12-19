4647ed3ae93d59f4baf3f98eb9da0c78b238f2ed
Mavafelly <matthewfeliciano16@gmail.com>
2024-12-19 15:37:33 -0500

    Footer Complete

--- src/components/Footer/foot.tsx
+++ src/components/Footer/foot.tsx
@@ -1,8 +1,8 @@
 
 const Footer = () => {
     return (
-      <footer className="flex justify-center items-center bg-gray-800 text-white py-4 fixed bottom-0 w-full">
-        <div className="flex gap-8">
+        <footer className="flex flex-col justify-center items-center bg-gray-800 text-white py-4 fixed bottom-0 w-full">
+        <div className="flex gap-8 mb-4">
           <a href="https://github.com/mavalfelly" className="text-4xl hover:text-gray-400">
             <ion-icon name="logo-github"></ion-icon>
           </a>
@@ -15,6 +15,12 @@ const Footer = () => {
           >
             <ion-icon name="logo-linkedin"></ion-icon>
           </a>
+        </div>
+  
+        <div id="build" className="text-center">
+          <p className="text-sm text-gray-400">
+            Built by Matt using React, TypeScript, JavaScript, Tailwind, and Django.
+          </p>
         </div>
       </footer>
     );
