12fd3b1baaba465c8e5c4b903876e8862135de9d
Mavafelly <matthewfeliciano16@gmail.com>
2024-12-26 20:42:56 -0500

    te

--- src/components/bckVid/Background.tsx
+++ src/components/bckVid/Background.tsx
@@ -4,17 +4,19 @@ import "./backVid.css";
 
 const BackgroundVideo: React.FC = () => {
   return (
-    <div className="video-container">
-      <div className="video-overlay" />
-      <video
-        muted
-        autoPlay
-        preload="true"
-        loop
-        className="bg-video"
-        src="/back.mp4"
-      />
-    </div>
+    <>
+      <div className="video-container">
+        <div className="video-overlay" />
+        <video
+          muted
+          autoPlay
+          preload="true"
+          loop
+          className="bg-video"
+          src="/back/back.mp4"
+        />
+      </div>
+    </>
   );
 };
 
