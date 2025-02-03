f29a1e517700a3b9ec76ac01e2597e00ac1600b0
Mavafelly <matthewfeliciano16@gmail.com>
2025-02-03 08:47:53 -0500

    UPDATED RES

--- src/components/AboutMe/aboutMe.tsx
+++ src/components/AboutMe/aboutMe.tsx
@@ -33,7 +33,7 @@ const AbMe = () => {
           </p>
         </div>
         <a
-            href="/Matt_Feliciano.pdf" 
+            href="/MatthewFeliciano.pdf" 
             download="Matt_Feliciano_Resume.pdf" 
             className="relative mt-4 px-4 md:px-6 py-2 text-black font-semibold border border-black max-w-32 group text-sm md:text-base"
             >
